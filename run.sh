505b96986d43b7fb0a12238a28b4e56b740d4e65
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2024-02-28 13:02:02 +0800

    Update run.sh
    New eval detection

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo ""
-echo "Universal Shell DEC 6.1"
+echo "Universal Shell DEC 6.2"
 dec() {
-  if grep -q -e 'eval' -e 'base64 -d | sh$' -e '" | sh' "$(pwd)/$shuf.temp1.sh"; then
+  if grep -q -e 'z="' -e 'base64 -d | sh$' -e '" | sh' "$(pwd)/$shuf.temp1.sh"; then
     while true; do
-      if grep eval "$(pwd)/$shuf.temp1.sh"; then
+      if grep 'z="' "$(pwd)/$shuf.temp1.sh"; then
         sed 's/eval "\$/echo "\$/g; s/\[ "$(id -u)" -ne 2000 \]/! true/' "$(pwd)/$shuf.temp1.sh" > "$(pwd)/$shuf.temp2.sh"
         bash "$(pwd)/$shuf.temp2.sh" > "$(pwd)/$shuf.temp1.sh"
         rm "$(pwd)/$shuf.temp2.sh"
@@ -60,7 +60,7 @@ find "$input" -maxdepth 1 -type f | while IFS= read -r file; do
   chmod +x "$(pwd)/$shuf.temp1.sh"
   echo "Decrypting $(basename "$file")"
   dec > /dev/null 2>&1
-  if grep -q -e 'eval' -e 'base64 -d | sh$' -e '" | sh' "$(pwd)/$shuf.temp1.sh"; then
+  if grep -q -e 'z="' -e 'base64 -d | sh$' -e '" | sh' "$(pwd)/$shuf.temp1.sh"; then
   dec > /dev/null 2>&1
   fi
   if [ -s "$(pwd)/$shuf.temp1.sh" ]; then
