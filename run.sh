90e0ee12059c29305ca9075e0c4f7ff483800c89
RiProG-id <81682107+RiProG-id@users.noreply.github.com>
2024-02-28 21:42:42 +0800

    Update run.sh
    Random hash support

--- run.sh
+++ run.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 ulimit -s unlimited >/dev/null 2>&1
 echo ""
-echo "Universal Shell DEC 6.6"
+echo "Universal Shell DEC 6.8"
 dec() {
-  if grep -q -e '=.*;.*=.*;' -e 'base64 -d | sh$' -e '" | sh' "$(pwd)/$shuf.temp1.sh"; then
+  if grep -q -e '=.*;.*=.*;' -e 'base64 -d | sh$' -e '" | sh' -e '*#*#*#*#*#*#*#*#*#*#*#*' "$(pwd)/$shuf.temp1.sh"; then
     while true; do
       if grep '=.*;.*=.*;' "$(pwd)/$shuf.temp1.sh"; then
         sed 's/eval "\$/echo "\$/g; s/\[ "$(id -u)" -ne 2000 \]/! true/' "$(pwd)/$shuf.temp1.sh" > "$(pwd)/$shuf.temp2.sh"
         bash "$(pwd)/$shuf.temp2.sh" > "$(pwd)/$shuf.temp1.sh"
         rm "$(pwd)/$shuf.temp2.sh"
       elif grep -q 'base64 -d | sh$' "$(pwd)/$shuf.temp1.sh"; then
-        sed 's/base64 -d | sh/base64 -d/; s/\[ "$(id -u)" -ne 2000 \]/! true/' "$(pwd)/$shuf.temp1.sh" > "$(pwd)/$shuf.temp2.sh"
-        bash "$(pwd)/$shuf.temp2.sh" > "$(pwd)/$shuf.temp1.sh"
-        rm "$(pwd)/$shuf.temp2.sh"
-        elif grep -q '" | sh' "$(pwd)/$shuf.temp1.sh"; then
-        sed 's/\" | sh/\" > \"\$(pwd)\/$shuf.temp1.sh\"/g; s/\[ "$(id -u)" -ne 2000 \]/! true/" "$(pwd)/$shuf.temp1.sh' > "$(pwd)/$shuf.temp2.sh"
-        bash "$(pwd)/$shuf.temp2.sh"
-        rm "$(pwd)/$shuf.temp2.sh"
+      sed 's/base64 -d | sh/base64 -d/; s/\[ "$(id -u)" -ne 2000 \]/! true/' "$(pwd)/$shuf.temp1.sh" > "$(pwd)/$shuf.temp2.sh"
+      bash "$(pwd)/$shuf.temp2.sh" > "$(pwd)/$shuf.temp1.sh"
+      rm "$(pwd)/$shuf.temp2.sh"
+      elif grep -q '" | sh' "$(pwd)/$shuf.temp1.sh"; then
+      sed 's/\" | sh/\" > \"\$(pwd)\/$shuf.temp1.sh\"/g; s/\[ "$(id -u)" -ne 2000 \]/! true/" "$(pwd)/$shuf.temp1.sh' > "$(pwd)/$shuf.temp2.sh"
+      bash "$(pwd)/$shuf.temp2.sh"
+      rm "$(pwd)/$shuf.temp2.sh"
+      elif grep -q '*#*#*#*#*#*#*#*#*#*#*#*' "$(pwd)/$shuf.temp1.sh"; then
+      cat "$(pwd)/$shuf.temp2.sh" | grep -v '#####' > "$(pwd)/$shuf.temp1.sh"
+      rm "$(pwd)/$shuf.temp2.sh"
       else
         break
       fi
@@ -63,7 +66,7 @@ find "$input" -maxdepth 1 -type f | while IFS= read -r file; do
   chmod +x "$(pwd)/$shuf.temp1.sh"
   echo "Decrypting $(basename "$file")"
   dec > /dev/null 2>&1
-  if grep -q -e '=.*;.*=.*;' -e 'base64 -d | sh$' -e '" | sh' "$(pwd)/$shuf.temp1.sh"; then
+  if grep -q -e '=.*;.*=.*;' -e 'base64 -d | sh$' -e '" | sh' -e '*#*#*#*#*#*#*#*#*#*#*#*' "$(pwd)/$shuf.temp1.sh"; then
   dec > /dev/null 2>&1
   fi
   if [ -s "$(pwd)/$shuf.temp1.sh" ]; then
